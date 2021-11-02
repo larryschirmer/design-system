e2888e3d360636198f9c361da57fbadb16c3bbab
Larry Schirmer <larryschirmer@gmail.com>
2021-11-02 20:32:47 +0000

    chore: publish components

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+export { default as AccordianList } from "./components/publish/AccordianList";
 export { default as Button } from "./components/publish/Button";
 export { default as Input } from "./components/publish/formElements/Input";
 export { default as RadioToggle } from "./components/publish/formElements/RadioToggle";
+export { default as InlineFreqGraph } from "./components/publish/InlineFreqGraph";
+export { default as WordCloud } from "./components/publish/WordCloud";

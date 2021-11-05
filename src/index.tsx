3486d366474eec2a62c650d2d80c3be66a323002
Larry Schirmer <larryschirmer@gmil.com>
2021-11-05 16:36:26 +0000

    chore: use storybook to run project

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,3 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
-import App from "./App";
-
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
-
 export { default as AccordianList } from "./components/publish/AccordianList";
 export { default as Button } from "./components/publish/Button";
 export { default as Input } from "./components/publish/formElements/Input";

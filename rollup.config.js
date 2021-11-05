6c57fde941ccbdba414680c202e0e010f2e95eda
Larry Schirmer <larryschirmer@gmil.com>
2021-11-05 16:59:25 +0000

    feat: rollup without deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,35 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import postcss from "rollup-plugin-postcss";
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 const config = {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm",
+      format: 'esm',
       sourcemap: true,
     },
   ],
   plugins: [
-    peerDepsExternal(),
+    excludeDependenciesFromBundle({
+      peerDependencies: true,
+      dependencies: true,
+    }),
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss({
-      extensions: [".css"],
+      extensions: ['.css'],
     }),
   ],
 };

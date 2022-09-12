a025a0f6a03c79feb1ec6f62bd5cfa4820beae79
Larry Schirmer <larryschirmer@gmail.com>
2022-09-12 20:55:15 +0000

    feat: enable per package builds

--- rollup.config.js
+++ rollup.config.js
@@ -3,35 +3,45 @@ import external from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
+import json from '@rollup/plugin-json';
+const { readdirSync, statSync } = require('fs');
+const { join } = require('path');
 
-export default [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        file: 'dist/index.js',
-        format: 'cjs',
-      },
-      {
-        file: 'dist/index.es.js',
-        format: 'es',
-        exports: 'named',
-      },
-    ],
-    plugins: [
-      commonjs({
-        include: /node_modules/,
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react'],
-        babelHelpers: 'bundled',
-      }),
-      external(),
-      resolve(),
-      typescript(),
-      terser(),
-    ],
-  },
-];
+const dirs = (p) => readdirSync(p).filter((f) => statSync(join(p, f)).isDirectory());
+const modules = dirs('src');
+
+const config = (moduleDir) => ({
+  input: `src/${moduleDir}/index.ts`,
+  output: [
+    {
+      file: `src/${moduleDir}/build/index.js`,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: `src/${moduleDir}/build/index.es.js`,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom', 'styled-components'],
+  plugins: [
+    external(),
+    url(),
+    json(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react'],
+      babelHelpers: 'bundled',
+    }),
+    resolve({ browser: true }),
+    typescript(),
+    terser(),
+  ],
+});
+
+export default (commandLineArgs) => {
+  if (commandLineArgs.hasOwnProperty('package')) return [config(commandLineArgs.package)];
+  return modules.map((m) => config(m));
+};
